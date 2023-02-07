ca53fa2497e83a3ff237996d093afab82dbc4347
unknown <SofienBenKtib>
2023-02-07 16:03:55 +0100

    UI_Updates

--- src/main/java/com/example/weatherappkotlin/WeatherService.kt
+++ src/main/java/com/example/weatherappkotlin/WeatherService.kt
@@ -1,12 +1,13 @@
 package com.example.weatherappkotlin
 
 import android.telecom.Call
+import com.google.gson.JsonObject
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 
 interface WeatherService {
 
-    @GET("?lat=44.34&lon=10.99&appid=7664f1d0777e2d62d0aa1cc9b12de67e")
-    fun getWeatherByCity(): retrofit2.Call<String>
+    @GET("?lat=35.7643&lon=10.813&appid=7664f1d0777e2d62d0aa1cc9b12de67e")
+    fun getWeatherByCity(): retrofit2.Call<JsonObject>
 }
